47b4688cfee1e0d3526b384c38c07c9893aa8ad8
Ozioma45 <oziomaegole@gmail.com>
2024-03-21 18:31:59 +0100

    creating my middlewares

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Authentication token is missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+const authorizeUser = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeUser };
