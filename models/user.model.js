8793026dc19b41fb54324860c31c40a94b1283ed
Ozioma45 <oziomaegole@gmail.com>
2024-03-23 13:46:10 +0100

    ensured method for user authentication

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,11 @@
+// models/user.model.js
+
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-  username: {
+  email: {
     type: String,
     required: true,
     unique: true,
@@ -16,6 +20,37 @@ const userSchema = new mongoose.Schema({
     default: "guest",
   },
 });
+
+// Method to hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Method to generate JWT token
+userSchema.methods.generateToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      role: this.role,
+    },
+    process.env.JWT_SECRET
+  );
+};
 
 const User = mongoose.model("User", userSchema);
 
